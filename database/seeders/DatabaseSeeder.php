779d7b7fda0a039fc402123143f47fcb3d5314cb
savasanisujal1 <128718122+savasanisujal1@users.noreply.github.com>
2026-04-06 10:58:47 +0530

    Fixed Role and Permission seeders and 403 error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,7 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            PermissionTableSeeder::class,
+        ]);
+        
+        $this->call([
+            CreateAdminUserSeeder::class,
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
